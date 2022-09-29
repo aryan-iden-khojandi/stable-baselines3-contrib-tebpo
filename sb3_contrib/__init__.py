b86012001b68fdaae05f25b3a7e17a143d2b523b
Andrew Zheng <atz@Andrews-MacBook-Pro.local>
2022-09-29 13:08:14 -0400

    Export TEBPO

--- sb3_contrib/__init__.py
+++ sb3_contrib/__init__.py
@@ -6,6 +6,7 @@ from sb3_contrib.ppo_recurrent import RecurrentPPO
 from sb3_contrib.qrdqn import QRDQN
 from sb3_contrib.tqc import TQC
 from sb3_contrib.trpo import TRPO
+from sb3_contrib.tebpo import TEBPO
 
 # Read version from file
 version_file = os.path.join(os.path.dirname(__file__), "version.txt")
